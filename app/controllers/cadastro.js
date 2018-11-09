995112ad9267bd03270db0325137fdb7e05dc60c
unknown <f.l.spelling@gmail.com>
2018-11-08 22:30:51 -0200

    .

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,21 @@
 module.exports.cadastro = function(app, req, res) {
+    res.render('cadastro', {validacao: {}});
+}
+
+module.exports.cadastrar = function(app, req, res) {
+    var dadosForm = req.body;
+
+    req.assert('nome', 'nome é obrigatório').notEmpty();
+    req.assert('usuario', 'usuario é obrigatório').notEmpty();
+    req.assert('senha', 'senha é obrigatório').notEmpty();
+    req.assert('casa', 'casa é obrigatório').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros) {
+        res.render('cadastro', {validacao: erros});
+        return;
+    }
+
     res.render('cadastro');
 }
