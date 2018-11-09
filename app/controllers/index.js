42b1f95338d4429c82056821fc4cc44e5ac01db8
FERNANDO LENTISCO SPELLING <fernandpel@cptm.info>
2018-11-09 11:47:56 -0300

    .

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,19 @@
-module.exports.index = function(application, req, res){
-	res.render('index');
+module.exports.index = function (application, req, res) {
+	res.render('index', { validacao: {}, dadosForm: {} });
+}
+
+module.exports.autenticar = function (application, req, res) {
+	var dadosForm = req.body;
+
+	req.assert('usuario', 'usuario é obrigatoório').notEmpty();
+	req.assert('senha', 'senha é obrigatoório').notEmpty();
+
+	var erros = req.validationErrors();
+
+	if (erros) {
+		res.render('index', { validacao: erros, dadosForm: dadosForm });
+		return;
+	}
+
+	res.send('podemos logar');
 }
